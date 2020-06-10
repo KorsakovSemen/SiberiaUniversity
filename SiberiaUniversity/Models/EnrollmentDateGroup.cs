78c34e45e29b4a18f84600278349c99588e761ee
pako2 <deacle2009@mail.ru>
2020-06-10 10:23:38 +0700

    added search, filter, pagination for students

---
+++ SiberiaUniversity/Models/EnrollmentDateGroup.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SiberiaUniversity.Models
+{
+    public class EnrollmentDateGroup
+    {
+        [DataType(DataType.Date)]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:d}")]
+        public DateTime? EnrollmentDate { get; set; }
+
+        public int StudentCount { get; set; }
+    }
+}
