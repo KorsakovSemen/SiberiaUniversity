5d21acbb5f70d2e5c16f06191133af09719a3d6e
pako2 <deacle2009@mail.ru>
2020-07-11 10:54:03 +0700

    added courses and instructors controllers

---
+++ SiberiaUniversity/Models/SchoolViewModels/InstructorIndexData.cs
@@ -0,0 +1,15 @@
+﻿using SiberiaUniversity.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SiberiaUniversity.Models.SchoolViewModels
+{
+    public class InstructorIndexData
+    {
+        public IEnumerable<Instructor> Instructors { get; set; }
+        public IEnumerable<Course> Courses { get; set; }
+        public IEnumerable<Enrollment> Enrollments { get; set; }
+    }
+}
